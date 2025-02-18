ee9e44cf0222ab31911caf57d1faf170b23d2fef
Macbook <macbook@Macbooks-MacBook-Pro.local>
2025-02-18 11:55:38 +0300

    first commit app file

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+// Create an express application
+const app = express();
+
+// Define the port
+const port = 3000;
+
+// Create a GET route
+app.get('/', (req, res) => {
+    res.send("<h1>Hello. This is the index</h1>");
+    console.log("Running Get / Route")
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
