08ef347582fc37431b0d765f85fdf2e002955a55
Casa Grande <casa.grande.et.senza@gmail.com>
2025-02-24 19:55:51 +0300

    Update

--- app.js
+++ app.js
@@ -3,16 +3,40 @@ const express = require('express');
 // Create an express application
 const app = express();
 
+//View engine
+app.set('view engine', 'ejs')
+
+
 // Define the port number local
 const port = 3000;
 
-// Create a GET route initials
+// GET routes --------------------------------------->
+//Index
 app.get('/', (req, res) => {
-    res.send("<h1>Hello. This is the bmma-app index</h1>");
-    console.log("Running Get / Route")
+    res.render('index');
+    console.log("Running Index")
 });
 
-// Start the server
+
+//404 View
+app.get('/404', (req, res) => {
+    res.render('404', {title: '404'});
+    console.log("Running 404")
+
+});
+
+//Campaigns Route
+app.get('/campaigns', (req, res) => {
+    res.render('campaigns', {title: 'Current Campaigns'});
+    console.log("Campaigns")
+
+});
+
+
+
+
+
+// Server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
